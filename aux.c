9e505995263180dc5b4009319c9d38be86693ad7
Mohsen Koohi Esfahani <mohsen@burge.ir>
2024-08-18 21:12:46 +0100

    xoshiro256 and splitmix64 randomization()

--- aux.c
+++ aux.c
@@ -451,4 +451,34 @@ char* ul2s(unsigned long in, char* out)
 	return out;
 }
 
+// Using splitmix64 (https://prng.di.unimi.it/splitmix64.c) to fill the seeds using the seed x
+void rand_initialize_splitmix64(unsigned long s[4], unsigned long x)
+{	
+	for(int si = 0; si < 4; si++)
+	{
+		unsigned long z = (x += 0x9e3779b97f4a7c15);
+		z = (z ^ (z >> 30)) * 0xbf58476d1ce4e5b9;
+		z = (z ^ (z >> 27)) * 0x94d049bb133111eb;
+		s[si] = z ^ (z >> 31);
+	}
+
+	return;
+}
+
+// XOSHIRO, https://prng.di.unimi.it/xoshiro256plusplus.c
+unsigned long rand_xoshiro256(unsigned long s[4]) 
+{
+	unsigned long rand_val = s[0] + (((s[0] + s[3]) << 23) | ((s[0] + s[3]) >> (64 - 23)));
+
+	unsigned long t = s[1] << 17;
+	s[2] ^= s[0];
+	s[3] ^= s[1];
+	s[1] ^= s[2];
+	s[0] ^= s[3];
+	s[2] ^= t;
+	s[3] = (s[3] << 45) | (s[3] >> (64 - 45));
+
+	return rand_val;
+}
+
 #endif
