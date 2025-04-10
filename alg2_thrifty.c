36b4434495579d27776cb15a1d90d5708bcfce6c
Mohsen Koohi Esfahani <mohsen@burge.ir>
2025-04-10 11:42:57 +0100

    alg2, validation

--- alg2_thrifty.c
+++ alg2_thrifty.c
@@ -73,84 +73,89 @@ int main(int argc, char** args)
 
 	// CC
 		unsigned int flags = 1U;  // 1U print stats
-		unsigned int ccs_p = 0;
-		unsigned int* cc_p = cc_pull(pe, graph, flags, exec_info, &ccs_p);
 		unsigned int ccs_t = 0;
 		unsigned int* cc_t = cc_thrifty_400(pe, graph, flags, &exec_info[10], &ccs_t);
 		unsigned int max_degree_ID  = exec_info[10 + 9];
 		
 	// Validating
-		// (1) If two vertices are on the same componenet (i.e., they have the same cc_p), they should have the same cc_t
-		#pragma omp parallel for
-		for(unsigned v = 0; v < graph->vertices_count; v++)
+		if(0)
 		{
-			assert(cc_t[v] == cc_t[cc_p[v]]);
-			
-			if(cc_t[v] == 0)
-				assert(cc_p[v] == cc_p[max_degree_ID]);
-			else
-				assert(cc_p[v] == cc_p[cc_t[v] - 1]);
-		}
+			unsigned int ccs_p = 0;
+			unsigned int* cc_p = cc_pull(pe, graph, flags, exec_info, &ccs_p);
 		
-		/* (2) It is also required to check if two vertices are on different components (i.e., not having the same cc_p), 
-		they do not have the same cc_t. This is checked by matching the total number of compoents. 
-		If two vertices are wrongly identified to have the same cc_t values, then either (i) we have 
-		different number of compoenents, or (ii) we have the same number of components. Now, we prove that 
-		(ii) cannot happen, i.e., (i) should be happened. If we have a correct number of components, and also
-		two vertices on different components but with the same cc_t, then we have to have a component that is splitted 
-		by cc_t, i.e., there are at least two vertices on the same component that their cc_t are different. This cannot happen 
-		as it has been checked by (1). So, by matching the number of components, we can be sure that two vertices on differnt 
-		components cannot be seen as connected by cc_t. 
-		*/
-		assert(ccs_p == ccs_t);
+			// (1) If two vertices are on the same componenet (i.e., they have the same cc_p), they should have the same cc_t
+			#pragma omp parallel for
+			for(unsigned v = 0; v < graph->vertices_count; v++)
+			{
+				assert(cc_t[v] == cc_t[cc_p[v]]);
+				
+				if(cc_t[v] == 0)
+					assert(cc_p[v] == cc_p[max_degree_ID]);
+				else
+					assert(cc_p[v] == cc_p[cc_t[v] - 1]);
+			}
+			
+			/* (2) It is also required to check if two vertices are on different components (i.e., not having the same cc_p), 
+			they do not have the same cc_t. This is checked by matching the total number of compoents. 
+			If two vertices are wrongly identified to have the same cc_t values, then either (i) we have 
+			different number of compoenents, or (ii) we have the same number of components. Now, we prove that 
+			(ii) cannot happen, i.e., (i) should be happened. If we have a correct number of components, and also
+			two vertices on different components but with the same cc_t, then we have to have a component that is splitted 
+			by cc_t, i.e., there are at least two vertices on the same component that their cc_t are different. This cannot happen 
+			as it has been checked by (1). So, by matching the number of components, we can be sure that two vertices on differnt 
+			components cannot be seen as connected by cc_t. 
+			*/
+			assert(ccs_p == ccs_t);
 
-		printf("Validation:\t\t\033[1;33mCorrect\033[0;37m\n");
+			printf("Validation:\t\t\033[1;33mCorrect\033[0;37m\n");
+			cc_release(graph, cc_p);
+			cc_p = NULL;
+		}
 
 	// Writing to the report
 		if(LL_OUTPUT_REPORT_PATH != NULL)
 		{
 			FILE* out = fopen(LL_OUTPUT_REPORT_PATH, "a");
-			assert(out != NULL);
-
-			if(LL_INPUT_GRAPH_BATCH_ORDER == 0)
+			if(out != NULL)
 			{
-				fprintf(out, "%-20s; %-8s; %-8s; %-13s;", "Dataset", "|V|", "|E|", "Time (ms)");
+				if(LL_INPUT_GRAPH_BATCH_ORDER == 0)
+				{
+					fprintf(out, "%-20s; %-8s; %-8s; %-13s;", "Dataset", "|V|", "|E|", "Time (ms)");
+					if(exec_info)
+						for(unsigned int i=0; i<pe->hw_events_count; i++)
+							fprintf(out, " %-8s;", pe->hw_events_names[i]);
+					fprintf(out, "\n");
+				}
+
+				char temp1 [16];
+				char temp2 [16];			
+				char* name = strrchr(LL_INPUT_GRAPH_PATH, '/');
+				if(name == NULL)
+					name = LL_INPUT_GRAPH_PATH;
+				else
+					name++;
+				if(strcmp(LL_INPUT_GRAPH_TYPE,"text") == 0)
+					name = strndup(name, strrchr(name, '.') - name);
+				
+				fprintf(out, "%-20s; %'8s; %'8s; %'13.1f;", 
+					name, ul2s(graph->vertices_count, temp1), ul2s(graph->edges_count, temp2), exec_info[10 + 0] / 1e6);
 				if(exec_info)
-					for(unsigned int i=0; i<pe->hw_events_count; i++)
-						fprintf(out, " %-8s;", pe->hw_events_names[i]);
+					for(unsigned int i=0; i<pe->hw_events_count; i++)					
+						fprintf(out, " %'8s;", ul2s(exec_info[10 + i + 1], temp1));
 				fprintf(out, "\n");
+
+				if(strcmp(LL_INPUT_GRAPH_TYPE,"text") == 0)
+					free(name);
+				name = NULL;
+
+				fclose(out);
+				out = NULL;
 			}
-
-			char temp1 [16];
-			char temp2 [16];			
-			char* name = strrchr(LL_INPUT_GRAPH_PATH, '/');
-			if(name == NULL)
-				name = LL_INPUT_GRAPH_PATH;
-			else
-				name++;
-			if(strcmp(LL_INPUT_GRAPH_TYPE,"text") == 0)
-				name = strndup(name, strrchr(name, '.') - name);
-			
-			fprintf(out, "%-20s; %'8s; %'8s; %'13.1f;", 
-				name, ul2s(graph->vertices_count, temp1), ul2s(graph->edges_count, temp2), exec_info[10 + 0] / 1e6);
-			if(exec_info)
-				for(unsigned int i=0; i<pe->hw_events_count; i++)					
-					fprintf(out, " %'8s;", ul2s(exec_info[10 + i + 1], temp1));
-			fprintf(out, "\n");
-
-			if(strcmp(LL_INPUT_GRAPH_TYPE,"text") == 0)
-				free(name);
-			name = NULL;
-
-			fclose(out);
-			out = NULL;
 		}
 
 	// Releasing memory
 		cc_release(graph, cc_t);
 		cc_t = NULL;
-		cc_release(graph, cc_p);
-		cc_p = NULL;
 		
 		if(LL_INPUT_GRAPH_IS_SYMMETRIC)
 		{
